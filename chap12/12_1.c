37b047bed591188e497a4319facc6db7256706b8
jeff8971 <mason8971@gmail.com>
2023-12-14 00:15:38 -0500

    finish 12_1.c

--- chap12/12_1.c
+++ chap12/12_1.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 
+void critics(int *u);
+int main(void){
+	int unit;
+
+	printf("How many pounds to a firkin of butter?\n");
+	scanf("%d", &unit);
+	while(unit != 56){
+		critics(&unit);
+	}
+	printf("You must have it look it up!\n");
+
+	return 0;
+}
+
+void critics(int *u){
+	printf("No luck, my friend. Try again.\n");
+	scanf("%d", u);
+}
+
+
