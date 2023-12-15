0a53db5f9533b4a343a0b7d366ebf670ec1d1dd2
jeff8971 <mason8971@gmail.com>
2023-12-15 00:24:16 -0500

    pe12-2a.h, pe12-2a.c

---
+++ chap12/pe12-2a.c
@@ -0,0 +1,27 @@
+// pe12-2a.c
+// compile with pe12-2b.c
+
+#include <stdio.h>
+#include "pe12-2a.h"
+
+static int mode;
+static double distance;
+static double feul;
+
+void set_mode(int mode){
+	if (mode == 0){
+		double distance_kilo;
+		printf("Enter distance traveled in kilometers:");
+		scanf("%f", &distance_kilo);
+		
+	}
+
+
+}
+
+
+void get_info(){
+	
+
+}
+
---
+++ chap12/pe12-2a.h
@@ -0,0 +1,10 @@
+// pe12-2a.h
+#ifndef PE12-2A_H
+#define PE12-2A_H
+
+void set_mode(mode);
+void get_info();
+void show_info();
+
+
+#endif
