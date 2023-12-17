25ed16d76f464c367e0b42e1bd006a5fb90ae299
jeff8971 <mason8971@gmail.com>
2023-12-17 14:46:04 -0500

    pe12-2.c finish

--- chap12/pe12-2a.c
+++ chap12/pe12-2a.c
@@ -6,22 +6,46 @@
 
 static int mode;
 static double distance;
-static double feul;
+static double fuel;
 
-void set_mode(int mode){
-	if (mode == 0){
-		double distance_kilo;
-		printf("Enter distance traveled in kilometers:");
-		scanf("%f", &distance_kilo);
-		
+void set_mode(int m){
+	if (m == 0){
+		mode = 0;
+	}
+	else if (m == 1){
+		mode = 1;
+	}
+	else{
+		printf("Invalid mode specified. Mode %d(%s) used.\n", mode, mode == 0 ? "metric" : "US");
+		mode = 0;
 	}
 
 
 }
 
-
+// get_info() prompts the user to enter distance traveled and fuel consumed.
 void get_info(){
-	
-
+	if (mode == 0){
+		printf("Enter distance traveled in kilometers:");
+		scanf("%lf", &distance);
+		printf("Enter fuel consumed in liters:");
+		scanf("%lf", &fuel);
+	}
+	else{
+		printf("Enter distance traveled in miles:");
+		scanf("%lf", &distance);
+		printf("Enter fuel consumed in gallons:");
+		scanf("%lf", &fuel);
+	}
 }
 
+
+// show_info() calculates and displays the fuel consumption.
+void show_info(){
+	if (mode == 0){
+		printf("Fuel consumption is %.2lf liters per 100 km.\n", fuel / distance * 100);
+	}
+	else{
+		printf("Fuel consumption is %.1lf miles per gallon.\n", distance / fuel);
+	}
+}
