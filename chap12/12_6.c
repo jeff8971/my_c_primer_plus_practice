d3484d4a92de29af727ea35b3707b9173aee0f55
Jeff <mason8971@gmail.com>
2023-12-21 12:49:42 -0500

    some_updates, 12.6

---
+++ chap12/12_6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 10
+#define MIN 1
+
+#define SIZE 1000
+
+extern int rand1(void);
+extern void srand1(unsigned int x);
+
+int main(void){
+
+  
+}
