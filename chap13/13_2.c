e24d3926d2a459acb445c283af1a2a806052d1d7
jeff8971 <mason8971@gmail.com>
2023-12-26 12:14:07 -0500

    13_2 test

--- chap13/13_2.c
+++ chap13/13_2.c
@@ -6,26 +6,39 @@
 
 
 int main(int argc, char *argv[]){
-  char ch;
+
+  int byte;
   FILE * origin;
   FILE * copy;
 
+  // error handling
   if (argc != 3){
     printf("Usage: ./copy [original file name] [copy file name]");
     exit(EXIT_FAILURE);
   }
 
-  if((origin = fopen("test.txt", "r")) == NULL){
+  if((origin = fopen(argv[1], "rb")) == NULL){
     printf("The original file did not exist.\n");
     exit(EXIT_FAILURE);
   }
 
-  
-  
+  if((copy = fopen(argv[2], "wb")) == NULL){
+    printf("The copy file can not be created.\n");
+    exit(EXIT_FAILURE);
+  }
 
 
+  while(byte = getc(origin) != EOF){
+    putc(byte, copy);
+  }
 
+  if (fclose(origin) != 0){
+    printf("Could not close the original file.\n");
+  }
 
+  if (fclose(copy) != 0){
+    printf("Could not close the copy file.\n");
+  }
 
-  return 0;
+  return 0;  
 }
