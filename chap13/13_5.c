eceffa1b042f44ce2c952b929a26f19a15f72327
jeff8971 <mason8971@gmail.com>
2024-01-04 00:10:59 -0500

    13_5 in progress

--- chap13/13_5.c
+++ chap13/13_5.c
@@ -11,15 +11,32 @@ interactive interface.
 #define BUFSIZE 4096
 #define SLEN 81
 void append(FILE *source, FILE *dest);
-char * s_gets(char * st, int n);
 
-int main(void)
+int main(int argc, char* argv[])
 {
     FILE *fa, *fs;    // fa for append file, fs for source file
     int files = 0;  // number of files appended
-    char file_app[SLEN];  // name of append file
-    char file_src[SLEN];  // name of source file
-    int ch;
+    int fct;
+
+    if (argc < 3){
+        printf("Usage: %s [append_file][source_file]", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if((fa = fopen(argv[1], "a") == NULL)){
+        fprintf(stderr, "Can not open %s.\n");
+        exit(EXIT_FAILURE);
+    }
+    if(setvbuf(fa, NULL, _IOFBF, BUFSIZE) != 0){
+        fputs("Can not create output buffer.\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    for (fct = 2; fct < argc; fct++){
+
+    }
+
+
 
     puts("Enter name of destination file:");
     s_gets(file_app, SLEN);
