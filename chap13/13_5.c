6d54a2623ba67d0e0cfa462744b15db5984ecb15
jeff8971 <mason8971@gmail.com>
2024-01-04 21:56:17 -0500

    13_5 finish

--- chap13/13_5.c
+++ chap13/13_5.c
@@ -1,42 +1,70 @@
-/*
-Modify the program in Listing 13.5
- so that it uses a command-line interface instead of an
-interactive interface.
-*/
+/* Programming Exercise 13-5.c */
 
-/* append.c -- appends files to a file */
+// Modify the program in Listing 13.5 so that it uses a command-line interface instead of an
+// interactive interface.
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define BUFSIZE 4096
 #define SLEN 81
 void append(FILE *source, FILE *dest);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    FILE *fa, *fs;    // fa for append file, fs for source file
-    int files = 0;  // number of files appended
+    FILE *fa, *fs;
+    int files = 0;
     int fct;
 
-    if (argc < 3){
-        printf("Usage: %s [append_file][source_file]", argv[0]);
+    if (argc < 3)
+    {
+        printf("Usage: %s appendfile sourcefile[s]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if((fa = fopen(argv[1], "a") == NULL)){
-        fprintf(stderr, "Can not open %s.\n");
-        exit(EXIT_FAILURE);
-    }
-    if(setvbuf(fa, NULL, _IOFBF, BUFSIZE) != 0){
-        fputs("Can not create output buffer.\n", stderr);
+    if ((fa = fopen(argv[1], "a")) == NULL)
+    {
+        fprintf(stderr, "Can't open %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
-    for (fct = 2; fct < argc; fct++){
-
+    if (setvbuf(fa, NULL, _IOFBF, BUFSIZE) != 0)
+    {
+        fputs("Can't create output buffer\n", stderr);
+        exit(EXIT_FAILURE);
     }
 
+    for (fct = 2; fct < argc; fct++)
+    {
+        if (strcmp(argv[fct], argv[1]) == 0)
+            fputs("Can't append file to itself\n", stderr);
+        else if ((fs = fopen(argv[fct], "r")) == NULL)
+            fprintf(stderr, "Can't open %s\n", argv[fct]);
+        else
+        {
+            if (setvbuf(fs, NULL, _IOFBF, BUFSIZE) != 0)
+            {
+                fputs("Can't create output buffer\n", stderr);
+                continue;
+            }
+            append(fs, fa);
+            if (ferror(fs) != 0)
+                fprintf(stderr, "Error in reading file %s.\n",
+                        argv[fct]);
+            if (ferror(fa) != 0)
+                fprintf(stderr, "Error in reading file %s.\n",
+                        argv[1]);
+            fclose(fs);
+            files++;
+            printf("File %s appended.\n", argv[fct]);
+        }
+    }
+    printf("Done. %d files appended.\n", files);
+    fclose(fa);
 
+    return 0;
 }
 
 void append(FILE *source, FILE *dest)
@@ -46,22 +74,4 @@ void append(FILE *source, FILE *dest)
 
     while ((bytes = fread(temp, sizeof(char), BUFSIZE, source)) > 0)
         fwrite(temp, sizeof(char), bytes, dest);
-}
-
-char * s_gets(char * st, int n)
-{
-    char * ret_val;
-    char * find;
-
-    ret_val = fgets(st, n, stdin);
-    if (ret_val)
-    {
-        find = strchr(st, '\n');   // look for newline
-        if (find)                  // if the address is not NULL
-            *find = '\0';          // place a null character there
-        else
-            while (getchar() == '\n')
-                continue;
-    }
-    return ret_val;
 }
